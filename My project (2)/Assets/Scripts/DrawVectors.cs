a7ad447db5f310652227e6ba2b45862b4fa369bd
Dora <doracheng540@gmail.com>
2026-09-10 18:59:21 -0400

    Added two lines in drawline

--- My project (2)/Assets/Scripts/DrawVectors.cs
+++ My project (2)/Assets/Scripts/DrawVectors.cs
@@ -11,6 +11,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 dVector= new Vector2 (0, 1);
+        Vector2 eVector = new Vector2(3, -2);
+        Debug.DrawLine(new Vector2(0, 0), dVector, Color.yellow);
+        Debug.DrawLine(new Vector2(0,0),eVector,Color.grey);
     }
 }
