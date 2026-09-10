80214c88831724d980b7fd6787a19d3704c1815f
Dora <doracheng540@gmail.com>
2026-09-10 19:44:48 -0400

    Added transforms

--- My project (2)/Assets/Scripts/AddVectors.cs
+++ My project (2)/Assets/Scripts/AddVectors.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class AddVectors : MonoBehaviour
 {
+    Transform rtransform;
+    Transform bTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SpriteRenderer R= GetComponent<SpriteRenderer>(); ;
         SpriteRenderer B= GetComponent<SpriteRenderer>(); ;
+        R.color = Color.red;
+        B.color = Color.blue;
     }
 
     // Update is called once per frame
