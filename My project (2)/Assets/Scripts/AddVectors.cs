ca8a261bbe8837b2b617376ad2f07e51a049421c
Dora <doracheng540@gmail.com>
2026-09-12 12:55:53 -0400

    Finish question 2 to 4

--- My project (2)/Assets/Scripts/AddVectors.cs
+++ My project (2)/Assets/Scripts/AddVectors.cs
@@ -1,7 +1,7 @@
 using System.Drawing;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+//Dora Cheng 2026-9-12
 public class AddVectors : MonoBehaviour
 {
     public Transform rTransform;
@@ -17,9 +17,17 @@ public class AddVectors : MonoBehaviour
     void Update()
     {
       Vector2 rPlusB= rTransform.position + bTransform.position;
-      if(Input.GetKeyDown (KeyCode.B))
+      if(Input.GetKeyDown (KeyCode.B))      //question 2
         {
             Debug.DrawLine(Vector2.zero, bTransform.position, UnityEngine.Color.blue);
+        }
+      if (Input.GetKeyDown(KeyCode.R))      //question 3
+      {
+          Debug.DrawLine(Vector2.zero, rTransform.position, UnityEngine.Color.red);
+      }
+      if( Input.GetKeyDown(KeyCode.R)&& Input.GetKeyDown(KeyCode.B))    //question 4
+        {
+            Debug.DrawLine(Vector2.zero, rPlusB, UnityEngine.Color.magenta);
         }
     }
     void InputValues()
