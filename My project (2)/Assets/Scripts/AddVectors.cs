eec1103fa33285a934555d630d0e6964909ec5fa
Dora <doracheng540@gmail.com>
2026-09-12 12:26:57 -0400

    Trying different ways to fix my codes

--- My project (2)/Assets/Scripts/AddVectors.cs
+++ My project (2)/Assets/Scripts/AddVectors.cs
@@ -1,23 +1,35 @@
+using System.Drawing;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AddVectors : MonoBehaviour
 {
-    public Transform rtransform;
+    public Transform rTransform;
     public Transform bTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpriteRenderer R= GetComponent<SpriteRenderer>(); ;
-        SpriteRenderer B= GetComponent<SpriteRenderer>(); ;
-        R.color = Color.red;
-        B.color = Color.blue;
-
+        InputValues();
+        transform.position = rTransform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-      
-        
+      Vector2 rPlusB= rTransform.position + bTransform.position;
+      if(Input.GetKeyDown (KeyCode.B))
+        {
+            Debug.DrawLine(Vector2.zero, bTransform.position, UnityEngine.Color.blue);
+        }
+    }
+    void InputValues()
+    {
+        if (GetComponent<SpriteRenderer>().color ==  UnityEngine.Color.red )
+        {
+            transform.position = rTransform.position;
+        }else if (GetComponent<SpriteRenderer>().color == UnityEngine.Color.blue)
+            {
+                transform.position = bTransform.position;
+            }
     }
 }
