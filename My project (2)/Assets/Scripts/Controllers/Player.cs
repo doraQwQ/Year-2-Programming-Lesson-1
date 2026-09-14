8a7dd27cfd58ea6b22732e1bfb76670ebee607ba
Dora <doracheng540@gmail.com>
2026-09-14 16:41:48 -0400

    Added a spawnbombatoffset

--- My project (2)/Assets/Scripts/Controllers/Player.cs
+++ My project (2)/Assets/Scripts/Controllers/Player.cs
@@ -5,13 +5,20 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    Sprite bomb;
     public List<Transform> asteroidTransforms;
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    public Vector2 spawnOffset;
     void Update()
     {
 
     }
+    void SpawnBombAtOffset(Vector3 offset)
+    {
+        Instansate(bomb, offset.x, offset.y);
+    }
+    //rEfrence https://discussions.unity.com/t/check-if-e-key-is-pressed-in-c/657221
 }
---
+++ My project (2)/Assets/Scripts/Playerdotcs.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Playerdotcs : MonoBehaviour
+{
+    Sprite bomb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
+//rEfrence https://discussions.unity.com/t/check-if-e-key-is-pressed-in-c/657221
