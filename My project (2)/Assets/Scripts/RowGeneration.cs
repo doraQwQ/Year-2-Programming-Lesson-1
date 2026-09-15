aae355ff5f8baa70912aee8ad4ca3840509c2b46
Dora <doracheng540@gmail.com>
2026-09-14 22:15:54 -0400

    Added the UI stuff still figuring out

---
+++ My project (2)/Assets/Scripts/RowGeneration.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RowGeneration : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
