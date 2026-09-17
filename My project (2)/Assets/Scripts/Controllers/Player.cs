11644241ed4e606fcbaabd1e7bbf6d724541ee72
Dora <doracheng540@gmail.com>
2026-09-17 10:12:52 -0400

    Reworked the spawning bomb

--- My project (2)/Assets/Scripts/Controllers/Player.cs
+++ My project (2)/Assets/Scripts/Controllers/Player.cs
@@ -13,7 +13,8 @@ public class Player : MonoBehaviour
     public Transform bombsTransform;
     public Vector3 bombOffset;
     public Vector2 spawnOffset;
-
+    public float x;
+    public float y;
     public GameObject bombInstantation;
     void Start()
     {
@@ -23,9 +24,32 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.B))    //spawn bomb at a random place near player
         {  //prevent the bomb spawning at player
+            x = Random.Range(-2f, 2f);
+            for (int i = 0; i < 10; i++)
+            {
+                y = Random.Range(-2f, 2f);
+                if (x > -0.75 && x < 0.75 &&( y < -0.75 || y > 0.75))    //x is within player, y has to be upper or below
+                {
+                    break;
+                }
+                else if(!(x > -0.75 && x < 0.75))     // x is outside player position, any y works
+                {
+                    break;
+                }
+                //if (!(randomLocation > -0.75 && randomLocation < 0.75))   //prevent it from being on player
+                //{
+                //    if (!placedValue)  // put value in x
+                //    {
+                //        x = randomLocation;
+                //        placedValue = true;
+                //    } else 
+                //    {
+                //        y = randomLocation; //put value in y
+                //    }
+                //}
+            }
 
-            
-            bombOffset = new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), 0f);
+            bombOffset = new Vector3(x,y, 0f);
             SpawnBombAtOffset(bombOffset);
         }
         if (Input.GetKeyDown(KeyCode.W))
@@ -36,7 +60,7 @@ public class Player : MonoBehaviour
     }
     void SpawnBombAtOffset(Vector3 offset)
     {
-        DestroyBomb();
+        //DestroyBomb();bbbbbb
         bombInstantation =Instantiate(bombPrefab, transform.position+offset, Quaternion.identity);
         Debug.Log("X:" + offset.x + "Y:" +offset.y);
     }
