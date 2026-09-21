f8146fe21d752fb921018b2c6fde85f9fd392ece
Dora <doracheng540@gmail.com>
2026-09-21 17:21:47 -0400

    added foreach example

--- My project (2)/Assets/Scripts/Zoo.cs
+++ My project (2)/Assets/Scripts/Zoo.cs
@@ -10,6 +10,13 @@ public class Zoo : MonoBehaviour
         animals.Add("Dinosaur");
         animals.Add("Tiger");
         animals.Add("Penguin");
+        animals.Add("Peacok");
+        animals.Remove("Dinosaur");
+
+        foreach(string currentAnimal in animals)//the string is tempoary
+        {
+            Debug.Log("Our next animal is " + currentAnimal);
+        }
     }
 
     // Update is called once per frame
