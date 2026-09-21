a2b91928b867c8622d4ea96bf932261a6442043f
Dora <doracheng540@gmail.com>
2026-09-21 17:57:31 -0400

    Foreach loop stuff

--- My project (2)/Assets/Scripts/Controllers/Stars.cs
+++ My project (2)/Assets/Scripts/Controllers/Stars.cs
@@ -10,5 +10,22 @@ public class Stars : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        drawConsetellation();
+    }
+    void drawConsetellation()
+    {
+        //foreach (Transform whatever in starTransforms)
+        //{
+        //    Debug.Log(whatever.positon)
+        //}
+        //if using this one, need to create addition storing points, then use them to calculate and somehow
+        //knowing at some point it is the end and stops the code????
+           
+        
+        for (int i = 0; i < starTransforms.Count-1; i++)
+        {
+            int x = 10;
+            Debug.DrawLine(starTransforms[i].position, starTransforms[i++].position);
+        }
     }
 }
