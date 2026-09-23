b76a109df64c62411e0fbaf8d3df432dc54eaf6b
Dora <doracheng540@gmail.com>
2026-09-23 18:05:44 -0400

    Added the mouse held code

--- My project (2)/Assets/Scripts/Pipeline.cs
+++ My project (2)/Assets/Scripts/Pipeline.cs
@@ -1,10 +1,11 @@
-using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Pipeline : MonoBehaviour
 {
-    public List <Vector3> mouseLocation;
+    public List<Vector3> mouseLocation = new List<Vector3>();
     float timer = 0f;
+    bool start = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,8 +15,20 @@ public class Pipeline : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)){
+
+        if (Input.GetMouseButtonDown(0))
+        {   //being held
             mouseLocation.Add(Input.mousePosition);
+
+        }
+        if (Input.GetMouseButtonUp(0)) //clicked
+        {
+            timer = 0f;
+            start = true;
+        }
+        if (start)
+        {
+            timer += Time.deltaTime;
         }
     }
 }
