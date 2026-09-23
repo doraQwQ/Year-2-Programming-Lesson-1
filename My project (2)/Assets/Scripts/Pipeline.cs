304a669a6721ce390ed352cead7381f8b4d84549
Dora <doracheng540@gmail.com>
2026-09-23 17:43:57 -0400

    Added code

--- My project (2)/Assets/Scripts/Pipeline.cs
+++ My project (2)/Assets/Scripts/Pipeline.cs
@@ -1,7 +1,10 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class Pipeline : MonoBehaviour
 {
+    public List <Vector3> mouseLocation;
+    float timer = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,8 @@ public class Pipeline : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)){
+            mouseLocation.Add(Input.mousePosition);
+        }
     }
 }
