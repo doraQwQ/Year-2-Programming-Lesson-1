326d0773dcd2acc8875010c88060e79acbdcf469
Dora <doracheng540@gmail.com>
2026-09-24 17:04:09 -0400

    Finish

--- My project (2)/Assets/Scripts/Pipeline.cs
+++ My project (2)/Assets/Scripts/Pipeline.cs
@@ -12,50 +12,64 @@ public class Pipeline : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        line.startWidth = 0.02f;
+        line.endWidth = 0.02f;
+        line.startColor = Color.white;
+        line.endColor = Color.white;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetMouseButton(0))
-        {   //being held
-            
+        if (Input.GetMouseButton(0)) //LEFT MOUSE KEY being held 
+        {   
             timer += Time.deltaTime;
+            //if (mouseLocation.Count > 2)
+            //{
+            //    one = Camera.main.ScreenToWorldPoint(mouseLocation[mouseLocation.Count - 1]);
+            //    two = Camera.main.ScreenToWorldPoint(mouseLocation[mouseLocation.Count - 2]);
+            //    for (int i = 0; i < mouseLocation.Count - 1; i++)
+            //    {
+            //        Debug.DrawLine(one, two);
+            //    }
+            //}
+            
             if (timer > 0.1f)
-            {
-                line.positionCount = mouseLocation.Count;
+            { 
                 mouseLocation.Add(Input.mousePosition);
 
-                Debug.Log("WAAAAA");
-                if (start&&mouseLocation.Count>2)  //draw from old to new
-                { 
-                    one = Camera.main.ScreenToWorldPoint(mouseLocation[mouseLocation.Count - 1]);
-                    start = !start;
-                    Debug.Log("WBBB");
-                    for(int i = 0; i < mouseLocation.Count - 1; i++)
-                    {
-                        line.SetPosition(mouseLocation.Count - 1, one);
-                    }
-                }
-                else  if(!start&& mouseLocation.Count > 2)      //draw from new to old  
+                if (start && mouseLocation.Count > 2)  //draw from old to new
                 {
                     one = Camera.main.ScreenToWorldPoint(mouseLocation[mouseLocation.Count - 1]);
-                    for (int i = 0; i < mouseLocation.Count - 1; i++)
-                    {
-                        line.SetPosition(mouseLocation.Count - 1, one);
-                    }
+                    two = Camera.main.ScreenToWorldPoint(mouseLocation[mouseLocation.Count - 2]);
                     start = !start;
+                    line.positionCount = mouseLocation.Count;
+                    line.SetPosition(mouseLocation.Count - 2, one);
+                    line.SetPosition(mouseLocation.Count - 1, two);
                     
+             
                 }
-                timer = 0; ;
+                else if (!start && mouseLocation.Count > 2)      //draw from new to old  
+                {
+                    one = Camera.main.ScreenToWorldPoint(mouseLocation[mouseLocation.Count - 1]);
+                    two = Camera.main.ScreenToWorldPoint(mouseLocation[mouseLocation.Count - 2]);
+                    line.positionCount = mouseLocation.Count;
+                    line.SetPosition(mouseLocation.Count - 2, two);
+                    line.SetPosition(mouseLocation.Count - 1, one);
+                    
+                    
+                    start = !start;
+                }
+
+                timer = 0; 
             }
         }
         if (Input.GetMouseButtonUp(0)) //clicked
         {
             mouseLocation.Clear();    
             timer = 0;
+            line.positionCount = 0;
             mouseLocation.Add(Input.mousePosition);
             Debug.Log(mouseLocation[mouseLocation.Count - 1]);
         }
