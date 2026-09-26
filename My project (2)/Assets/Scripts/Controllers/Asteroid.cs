8ceaccb17b7f818dfe8f44cb9b6694122b66f464
Dora <doracheng540@gmail.com>
2026-09-26 12:47:32 -0400

    updates

--- My project (2)/Assets/Scripts/Controllers/Asteroid.cs
+++ My project (2)/Assets/Scripts/Controllers/Asteroid.cs
@@ -8,10 +8,11 @@ public class Asteroid : MonoBehaviour
     public float moveSpeed;
     public float arrivalDistance;
     public float maxFloatDistance;
-
+    Vector3 Vector, realLocation;
     // Start is called before the first frame update
     void Start()
     {
+        arrivalDistance = 0;
     }
 
     // Update is called once per frame
@@ -20,17 +21,17 @@ public class Asteroid : MonoBehaviour
     }
     public void AsteroidMovement()
     {
-        float a = Random.Range(-5, 5);
-        float b = Random.Range(-5, 5);
-        Vector3 newLocation= new Vector3(a, b, 0);
-        Vector3 Vector, realLocation ;
-        Vector = newLocation.normalized* maxFloatDistance*Time.deltaTime;
-        arrivalDistance = realLocation - transform.position;
         if (arrivalDistance < 5)
         {   //if close enough, find a new location to go to
-
+            float a = Random.Range(-10, 10);
+            float b = Random.Range(-10, 10);
+            newLocation = new Vector3(a, b, 0);
         }
-        transform.position += moveSpeed * Time.deltaTime;
+
+        Vector = newLocation.normalized* maxFloatDistance*Time.deltaTime;
+        arrivalDistance = realLocation - transform.position;
+        
+        transform.position += vector * Time.deltaTime;
 
 
     }
